aa856f309e85048b2475b54a3cfb543f1df1338a
r.verjus <romuald.verjus@viveris.fr>
2019-05-17 11:16:21 +0200

    add chalk to colorize outputs in the terminal

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,12 @@
 const fs = require('fs')
 const utils = require('./utils.js')
+const validator = require('validator')
+const chalk = require('chalk')
 
 fs.writeFileSync("notes.txt", utils.name + "\n")
 
 fs.appendFileSync("notes.txt", "a second message");
 
-console.log(utils.add(1, 2))
+
+const colorize = chalk.blue.underline 
+console.log(colorize(utils.add(1, 2)))
---
+++ notes-app/notes.js
@@ -0,0 +1,5 @@
+const getNotes = () => "youNotes ..."
+
+module.exports = {
+    getNotes
+};
